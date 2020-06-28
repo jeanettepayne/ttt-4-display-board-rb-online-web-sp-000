08d9fe36788c445452d4fda946ceb8db01970222
Jeanette Payne <jeanetterpayne@gmail.com>
2020-06-28 16:08:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,6 +12,7 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " #{board[7]}  | #{board[8]}  | #{board[9]}  "
 end
 
+display_board(board)
 
 board = [" ", " ", " ", " ", " ", "X", " ", " ", " "]
 display_board(board)
